088e412d26dd7d50fc9a050d148b1694fd17ecca
Rufina Talalaeva <rufusnorbert14@gmail.com>
2022-10-27 11:03:48 +0300

    feat(yandexcloud): add cloud config

--- drivers/yandexcloud/create.go
+++ drivers/yandexcloud/create.go
@@ -88,6 +88,41 @@ func (p *provider) createInstance(
 	if p.dockerComposeMetadata != "" {
 		metadata["docker-compose"] = p.dockerComposeMetadata
 	}
+	metadata["user-data"]= `#cloud-config
+	write_files:
+	  - path: /etc/systemd/system/docker.service.d/override.conf
+		content: |
+		  [Service]
+		  ExecStart=
+		  ExecStart=/usr/bin/dockerd
+	  - path: /etc/default/docker
+		content: |
+		  DOCKER_OPTS=""
+	  - path: /etc/docker/daemon.json
+		content: |
+		  {
+			"dns": [ "8.8.8.8", "8.8.4.4" ],
+			"hosts": [ "0.0.0.0:2376", "unix:///var/run/docker.sock" ],
+			"tls": true,
+			"tlsverify": true,
+			"tlscacert": "/etc/docker/ca.pem",
+			"tlscert": "/etc/docker/server-cert.pem",
+			"tlskey": "/etc/docker/server-key.pem"
+		  }
+	  - path: /etc/docker/ca.pem
+		encoding: b64
+		content: {{ .CACert | base64 }}
+	  - path: /etc/docker/server-cert.pem
+		encoding: b64
+		content: {{ .TLSCert | base64 }}
+	  - path: /etc/docker/server-key.pem
+		encoding: b64
+		content: {{ .TLSKey | base64 }}
+	
+	runcmd:
+	  - [ systemctl, daemon-reload ]
+	  - [ systemctl, restart, docker ]
+	`
 
 	request := &compute.CreateInstanceRequest{
 		FolderId:   folderID,
