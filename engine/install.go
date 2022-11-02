cbce7111d274c4c4be74a0dfad47d9678d461f6e
Rufina Talalaeva <rufusnorbert14@gmail.com>
2022-11-02 17:40:05 +0300

    fix: yet another try

--- engine/install.go
+++ engine/install.go
@@ -255,6 +255,8 @@ poller:
 	if err != nil {
 		i.metrics.IncrServerSetupError()
 		logger.WithField("image", i.image).
+			WithField("volumes", volumes).
+			WithField("mounts", mounts).
 			WithError(err).
 			Errorln("cannot create agent container")
 		return i.errorUpdate(ctx, instance, err)
