8cbb2af8fbbae5dabbd78152fcc1030c114786e5
Rufina Talalaeva <rufusnorbert14@gmail.com>
2022-11-08 00:56:47 +0300

    fix: indentations

--- drivers/yandexcloud/create.go
+++ drivers/yandexcloud/create.go
@@ -90,39 +90,39 @@ func (p *provider) createInstance(
 	}
 	metadata["user-data"] = `#cloud-config
 write_files:
-- path: /etc/systemd/system/docker.service.d/override.conf
-  permissions: '0644'
-  content: |
-  [Service]
-  ExecStart=
-  ExecStart=/usr/bin/dockerd
-- path: /etc/default/docker
-  permissions: '0644'
-  content: |
-  DOCKER_OPTS=""
-- path: /etc/docker/daemon.json
-  permissions: '0644'
-  content: |
-  {
-    "hosts": [ "0.0.0.0:2376", "unix:///var/run/docker.sock" ],
-    "tls": true,
-    "tlsverify": true,
-    "tlscacert": "/etc/docker/ca.pem",
-    "tlscert": "/etc/docker/server-cert.pem",
-    "tlskey": "/etc/docker/server-key.pem"
-  }
-- path: /etc/docker/ca.pem
-  permissions: '0644'
-  encoding: b64
-  content: {{ .CACert | base64 }}
-- path: /etc/docker/server-cert.pem
-  permissions: '0644'
-  encoding: b64
-  content: {{ .TLSCert | base64 }}
-- path: /etc/docker/server-key.pem
-  permissions: '0644'
-  encoding: b64
-  content: {{ .TLSKey | base64 }}
+  - path: /etc/systemd/system/docker.service.d/override.conf
+    permissions: '0644'
+    content: |
+    [Service]
+    ExecStart=
+    ExecStart=/usr/bin/dockerd
+  - path: /etc/default/docker
+    permissions: '0644'
+    content: |
+    DOCKER_OPTS=""
+  - path: /etc/docker/daemon.json
+    permissions: '0644'
+    content: |
+    {
+      "hosts": [ "0.0.0.0:2376", "unix:///var/run/docker.sock" ],
+      "tls": true,
+      "tlsverify": true,
+      "tlscacert": "/etc/docker/ca.pem",
+      "tlscert": "/etc/docker/server-cert.pem",
+      "tlskey": "/etc/docker/server-key.pem"
+    }
+  - path: /etc/docker/ca.pem
+    permissions: '0644'
+    encoding: b64
+    content: {{ .CACert | base64 }}
+  - path: /etc/docker/server-cert.pem
+    permissions: '0644'
+    encoding: b64
+    content: {{ .TLSCert | base64 }}
+  - path: /etc/docker/server-key.pem
+    permissions: '0644'
+    encoding: b64
+    content: {{ .TLSKey | base64 }}
 
 runcmd:
   - sudo systemctl daemon-reload
