486990a24f4c827ff56e380f935168a8f2fc3319
Rufina Talalaeva <rufusnorbert14@gmail.com>
2022-11-08 13:35:14 +0300

    fix: use another cloud-init

--- drivers/yandexcloud/userdata.go
+++ drivers/yandexcloud/userdata.go
@@ -3,43 +3,24 @@ package yandexcloud
 import "github.com/drone/autoscaler/drivers/internal/userdata"
 
 var userdataT = userdata.Parse(`#cloud-config
-#cloud-config
+
 write_files:
-  - path: /etc/systemd/system/docker.service.d/override.conf
-    permissions: '0644'
-    content: |
-    [Service]
-    ExecStart=
-    ExecStart=/usr/bin/dockerd
-  - path: /etc/default/docker
-    permissions: '0644'
-    content: |
-    DOCKER_OPTS=""
-  - path: /etc/docker/daemon.json
-    permissions: '0644'
-    content: |
-    {
-      "hosts": [ "0.0.0.0:2376", "unix:///var/run/docker.sock" ],
-      "tls": true,
-      "tlsverify": true,
-      "tlscacert": "/etc/docker/ca.pem",
-      "tlscert": "/etc/docker/server-cert.pem",
-      "tlskey": "/etc/docker/server-key.pem"
-    }
   - path: /etc/docker/ca.pem
-    permissions: '0644'
+    permissions: '0444'
     encoding: b64
     content: {{ .CACert | base64 }}
-  - path: /etc/docker/server-cert.pem
-    permissions: '0644'
+  - path: /etc/docker/cert.pem
+    permissions: '0444'
     encoding: b64
     content: {{ .TLSCert | base64 }}
-  - path: /etc/docker/server-key.pem
-    permissions: '0644'
+  - path: /etc/docker/key.pem
+    permissions: '0400'
     encoding: b64
     content: {{ .TLSKey | base64 }}
 
 runcmd:
+  - sudo sed -i "s/ExecStart=\\/usr\\/bin\\/dockerd/ExecStart=\\/usr\\/bin\\/dockerd -H tcp:\\/\\/0.0.0.0:2376 --tlsverify --tlscacert=\\/etc\\/docker\\/ca.pem --tlskey=\\/etc\\/docker\\/key.pem --tlscert=\\/etc\\/docker\\/cert.pem/g" /lib/systemd/system/docker.service
+  - sudo sed -i "s/DOCKER_OPTS=.*/DOCKER_OPTS=\"-H tcp:\\/\\/0.0.0.0:2376\"/g" /etc/init.d/docker
   - sudo systemctl daemon-reload
   - sudo systemctl restart docker.service
 `)
